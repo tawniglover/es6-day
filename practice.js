9c07c47f6cbf23c9b6c2655a03ffa23dc30e1663
Tawni Glover <equinoxjenkins@gmail.com>
2017-06-24 10:54:50 -0600

    halfway

--- practice.js
+++ practice.js
@@ -6,21 +6,26 @@
 
 // Rewrite the following line so that inner is scoped to the if statement.
 if ( true ) {
-	var inner = "Inner!";
+	const inner = "Inner!";
 }
 
 // declare a variable named noChange that cannot be re-assigned and give it a value of 100.
 
+const noChange = 'No change';
+
 
 // declare a new constant named vocab with the value of [ "let", "var", "destructuring", "spread", "rest" ]
+
+const vocab =  ["let", "var", "destructuring", "spread", "rest"];
 
 
 // change "var" to "const" at index 1 of vocab
 
+vocab[1] = 'const';
 
 // add "arrow function" to the end of vocab
 
-
+vocab.push('arrow function')
 
 ///////////////////////
 /* TEMPLATE LITERALS */
@@ -28,11 +33,13 @@ if ( true ) {
 
 // Declare 2 new variables, first and last, and assign them your first and last name.
 
+let first = 'Tawni';
 
+let last = 'Glover';
 
 // Declare the variable fullName and set it equal to your full name using template literals
 
-
+let fullName = `${first} ${last}`;
 
 //////////////////////
 /* OBJECT LITERALS */
@@ -43,10 +50,22 @@ if ( true ) {
 // and lovesDevMountain should be set to true.
 // learnES6 should be a method created with object method shorthand that returns "This is awesome!"
 
-
+let me = {
+	first,
+	last,
+	learnES6: function () {
+		return 'This is awesome!'
+	} ,
+	lovesDevMountain: true
+}
 
 // using computed property names create a new object named flipped where the keys are your first and last name
 // and the values are "first" and "last"
+
+let flipped = {
+	[me.first]: 'first',
+	[me.last]: 'last'
+}
 
 
 //////////////////////////////
@@ -63,7 +82,7 @@ const destructuring = {
 // using assignment destructuring create two new variables, objects and arrays,
 // with the values taken from the above object
 
-
+var {objects, arrays} = destructuring;
 
 
 // do not modify
